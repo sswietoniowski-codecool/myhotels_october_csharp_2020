e6adb0fc4275d3b8951bccb1db76106789452fbd
Sławomir Świętoniowski <slawomir.swietoniowski@codecool.com>
2021-07-03 15:58:53 +0200

    Implemented logging with SeriLog ;-)...

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
 
 namespace MyHotels.WebApi
 {
@@ -13,11 +15,30 @@ namespace MyHotels.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration().WriteTo.File(
+                path: @"logs\log-.txt",
+                outputTemplate: "{Timestamp:yyyy-mm-dd HH:mm:ss.fff zzz} {Level:u3} {Message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: LogEventLevel.Information).CreateLogger();
+
+            try
+            {
+                Log.Information("Application is starting...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                Log.Fatal(exception, "Application failed to start!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
