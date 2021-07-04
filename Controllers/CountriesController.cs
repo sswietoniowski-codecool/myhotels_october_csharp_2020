0fd019d912c5089445ddbe7ac836131a307856a8
Sławomir Świętoniowski <slawomir.swietoniowski@codecool.com>
2021-07-04 11:06:22 +0200

    Implemented CountriesController and GetCountries

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyHotels.WebApi.Infrastructure;
+using MyHotels.WebApi.Models;
+
+namespace MyHotels.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+        private readonly ILogger<CountriesController> _logger;
+
+        public CountriesController(IUnitOfWork uow, IMapper mapper, ILogger<CountriesController> logger)
+        {
+            this._uow = uow;
+            this._mapper = mapper;
+            this._logger = logger;
+        }
+
+        // GET ... api/countries
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IList<CountryDto>>> GetCountries()
+        {
+            _logger.LogInformation($"{nameof(GetCountries)} called...");
+
+            try
+            {
+                var countries = await _uow.Countries.GetAll();
+                var results = _mapper.Map<IList<CountryDto>>(countries);
+
+                return Ok(results);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, $"Something went wrong in {nameof(GetCountries)}");
+
+                //return StatusCode(StatusCodes.Status500InternalServerError,
+                //    "Internal server error, please try again later...");
+
+                return Problem("Internal server error, please try again later...");
+            }
+        }
+    }
+}
