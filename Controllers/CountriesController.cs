e973542b92a2d085643aec063b876ee788477fb7
Sławomir Świętoniowski <slawomir.swietoniowski@codecool.com>
2021-07-04 11:28:54 +0200

    Implemented GetCountry

--- Controllers/CountriesController.cs
+++ Controllers/CountriesController.cs
@@ -52,5 +52,39 @@ namespace MyHotels.WebApi.Controllers
                 return Problem("Internal server error, please try again later...");
             }
         }
+
+        // GET ... api/countries/1
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<CountryDto>> GetCountry(int id)
+        {
+            _logger.LogInformation($"{nameof(GetCountry)} called...");
+
+            try
+            {
+                var country = await _uow.Countries.Get(c => c.Id == id, new List<string> { "Hotels"});
+
+                if (country == null)
+                {
+                    return NotFound($"Not found country with id = {id}");
+                }
+
+                var result = _mapper.Map<CountryDto>(country);
+
+                return Ok(result);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, $"Something went wrong in {nameof(GetCountry)}");
+
+                //return StatusCode(StatusCodes.Status500InternalServerError,
+                //    "Internal server error, please try again later...");
+
+                return Problem("Internal server error, please try again later...");
+            }
+        }
+
     }
 }
